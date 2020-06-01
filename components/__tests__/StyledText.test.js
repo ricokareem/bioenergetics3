0783169bd7f2bc1f1aa3fabc73d61122acc97147
Rico Rodriquez Collins <rico@carbonfive.com>
2020-06-01 01:56:23 -0700

    first couple of tests

---
+++ components/__tests__/StyledText.test.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { MonoText } from '../../components/StyledText';
+
+describe('<MonoText>', () => {
+  describe('Rendering', () => {
+      it('should match to snapshot', () => {
+          const component = shallow(<MonoText>Snapshot test!</MonoText>)
+          expect(component).toMatchSnapshot()
+      });
+  });
+});
