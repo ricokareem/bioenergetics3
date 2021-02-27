dd0adf28f8dabb0c0d157ada0fce72fc9caae658
Rico Rodriquez Collins <rico@carbonfive.com>
2021-02-27 10:39:33 -0800

    ESLint: Calls to require() should use string literals (import/no-dynamic-require) pt2

---
+++ constants/VideoSourceFiles.ts
@@ -0,0 +1,24 @@
+/* eslint-disable global-require */
+
+const VideoSourceFiles = {
+  file1: require("../media/1.mp4"),
+  file2: require("../media/2.mp4"),
+  file3: require("../media/3.mp4"),
+  file4: require("../media/4.mp4"),
+  file5: require("../media/5.mp4"),
+  file6: require("../media/6.mp4"),
+  file7: require("../media/7.mp4"),
+  file9: require("../media/9.mp4"),
+  file10: require("../media/10.mp4"),
+  file11: require("../media/11.mp4"),
+  file81: require("../media/81.mp4"),
+  file82: require("../media/82.mp4"),
+  file121: require("../media/121.mp4"),
+  file122: require("../media/122.mp4"),
+  file123: require("../media/123.mp4"),
+  file131: require("../media/131.mp4"),
+  file132: require("../media/132.mp4"),
+  file133: require("../media/133.mp4"),
+};
+
+export default VideoSourceFiles;
--- screens/CardMedia.tsx
+++ screens/CardMedia.tsx
@@ -1,71 +1,86 @@
-import React from 'react';
-import { ScrollView, Text, StyleSheet } from 'react-native';
-import { Card, Button, Icon } from 'react-native-elements';
-import { Asset } from 'expo-asset';
-import { Video } from 'expo-av';
+import React from "react";
+import { ScrollView, Text, StyleSheet, Route } from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
+import { Asset } from "expo-asset";
+import { Video } from "expo-av";
 
-const CardMedia = (props) => {
+type CardMediaProps = {
+  route: Route;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fc9829",
+  },
+  backgroundVideo: {
+    width: "100%",
+    height: "40%",
+  },
+});
+
+const CardMedia = (props: CardMediaProps) => {
   const { route } = props;
   const { title, name, description } = route.params;
 
   let videoFile;
   switch (name) {
-  case 'card1':
-    videoFile = require('../media/1.mp4');
-    break;
-  case 'card2':
-    videoFile = require('../media/2.mp4');
-    break;
-  case 'card3':
-    videoFile = require('../media/3.mp4');
-    break;
-  case 'card4':
-    videoFile = require('../media/4.mp4');
-    break;
-  case 'card5':
-    videoFile = require('../media/5.mp4');
-    break;
-  case 'card6':
-    videoFile = require('../media/6.mp4');
-    break;
-  case 'card7':
-    videoFile = require('../media/7.mp4');
-    break;
-  case 'card81':
-    videoFile = require('../media/81.mp4');
-    break;
-  case 'card82':
-    videoFile = require('../media/82.mp4');
-    break;
-  case 'card9':
-    videoFile = require('../media/9.mp4');
-    break;
-  case 'card10':
-    videoFile = require('../media/10.mp4');
-    break;
-  case 'card11':
-    videoFile = require('../media/11.mp4');
-    break;
-  case 'card121':
-    videoFile = require('../media/121.mp4');
-    break;
-  case 'card122':
-    videoFile = require('../media/122.mp4');
-    break;
-  case 'card123':
-    videoFile = require('../media/123.mp4');
-    break;
-  case 'card131':
-    videoFile = require('../media/131.mp4');
-    break;
-  case 'card132':
-    videoFile = require('../media/132.mp4');
-    break;
-  case 'card133':
-    videoFile = require('../media/133.mp4');
-    break;
-  default:
-    break;
+    case "card1":
+      videoFile = require("../media/1.mp4");
+      break;
+    case "card2":
+      videoFile = require("../media/2.mp4");
+      break;
+    case "card3":
+      videoFile = require("../media/3.mp4");
+      break;
+    case "card4":
+      videoFile = require("../media/4.mp4");
+      break;
+    case "card5":
+      videoFile = require("../media/5.mp4");
+      break;
+    case "card6":
+      videoFile = require("../media/6.mp4");
+      break;
+    case "card7":
+      videoFile = require("../media/7.mp4");
+      break;
+    case "card81":
+      videoFile = require("../media/81.mp4");
+      break;
+    case "card82":
+      videoFile = require("../media/82.mp4");
+      break;
+    case "card9":
+      videoFile = require("../media/9.mp4");
+      break;
+    case "card10":
+      videoFile = require("../media/10.mp4");
+      break;
+    case "card11":
+      videoFile = require("../media/11.mp4");
+      break;
+    case "card121":
+      videoFile = require("../media/121.mp4");
+      break;
+    case "card122":
+      videoFile = require("../media/122.mp4");
+      break;
+    case "card123":
+      videoFile = require("../media/123.mp4");
+      break;
+    case "card131":
+      videoFile = require("../media/131.mp4");
+      break;
+    case "card132":
+      videoFile = require("../media/132.mp4");
+      break;
+    case "card133":
+      videoFile = require("../media/133.mp4");
+      break;
+    default:
+      break;
   }
 
   const chosenVideoURI = Asset.fromModule(videoFile).uri;
@@ -85,28 +100,20 @@ const CardMedia = (props) => {
           isLooping
           style={styles.backgroundVideo}
         />
-        <Text style={{marginBottom: 10}}>
-          {description}
-        </Text>
+        <Text style={{ marginBottom: 10 }}>{description}</Text>
         <Button
-          icon={<Icon name='code' color='#ffffff' />}
-          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-          title='NOW'
+          icon={<Icon name="code" color="#ffffff" />}
+          buttonStyle={{
+            borderRadius: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            marginBottom: 0,
+          }}
+          title="NOW"
         />
       </Card>
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fc9829',
-  },
-  backgroundVideo: {
-    width: '100%',
-    height: '40%',
-  },
-});
 
 export default CardMedia;
