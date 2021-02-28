246a6b9794c50ffd1eda6c9d29afabba52da941a
Rico Rodriquez Collins <rico@carbonfive.com>
2021-02-27 20:28:17 -0800

    eslint cleanup pt2

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
 
-describe('<App>', () => {
-  it('should match to snapshot', () => {
+describe("<App>", () => {
+  it("should match to snapshot", () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
--- screens/__tests__/CardScreen.test.tsx
+++ screens/__tests__/CardScreen.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CardScreen from '../CardScreen';
+import React from "react";
+import { shallow } from "enzyme";
+import CardScreen from "../CardScreen";
 
-describe('<CardScreen>', () => {
-  it('should match to snapshot', () => {
+describe("<CardScreen>", () => {
+  it("should match to snapshot", () => {
     const wrapper = shallow(<CardScreen />);
     expect(wrapper).toMatchSnapshot();
   });
