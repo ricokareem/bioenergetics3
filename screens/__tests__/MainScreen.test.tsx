17af0c807f355bdb35bed00a8064a222fa7f538b
Rico Rodriquez Collins <rico@carbonfive.com>
2022-02-13 15:19:23 -0800

    adding more tests

--- screens/__tests__/MainScreen.test.tsx
+++ screens/__tests__/MainScreen.test.tsx
@@ -4,8 +4,17 @@ import MainScreen from "../MainScreen";
 
 describe("<MainScreen>", () => {
   it("should match snapshot", () => {
-    const { toJSON } = render(<MainScreen navigation={{ id: "test" }} />);
+    const { toJSON, getAllByTestId, getByText } = render(
+      <MainScreen navigation={{ id: "test" }} />
+    );
+    const ListItemButtons = getAllByTestId("padView");
 
+    expect(ListItemButtons).toHaveLength(5);
+    expect(getByText(/healing sequences/i));
+    expect(getByText(/symptoms and conditions/i));
+    expect(getByText(/how to use this app/i));
+    expect(getByText(/beginning the healing session/i));
+    expect(getByText(/about the healing techniques/i));
     expect(toJSON()).toMatchSnapshot();
   });
 });
--- screens/__tests__/ReferenceGuideScreen.test.tsx
+++ screens/__tests__/ReferenceGuideScreen.test.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { map } from "lodash";
 import { render } from "@testing-library/react-native";
 import ReferenceGuideScreen from "../ReferenceGuideScreen";
+import { SymptomsPlaylists } from "../../constants/MediaData";
 
 describe("<ReferenceGuideScreen>", () => {
   it("should match snapshot", () => {
-    const { toJSON } = render(
+    const { toJSON, getAllByTestId, getByText } = render(
       <ReferenceGuideScreen navigation={{ id: "test" }} />
     );
+    const ListItemButtons = getAllByTestId("padView");
+    const titles = map(SymptomsPlaylists, (playlist) => playlist.title);
 
+    expect(ListItemButtons).toHaveLength(15);
+    titles.forEach((title) => {
+      expect(getByText(title));
+    });
     expect(toJSON()).toMatchSnapshot();
   });
 });
