eeeee12f22ed41f666f72d6c52cb435fc5adfa64
Rico Rodriquez Collins <rico@carbonfive.com>
2022-10-16 12:32:54 -0700

    mock out fontsLoaded for test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { render, waitFor } from "@testing-library/react-native";
 import App from "../App";
+import { isLoaded, useFonts } from "expo-font";
+
+jest.mock("expo-font");
 
 describe("<App>", () => {
+  beforeEach(() => {
+    useFonts.mockReturnValue([isLoaded, true]);
+  });
   it("should match snapshot", async () => {
     const { toJSON } = await waitFor(() => render(<App />));
 
