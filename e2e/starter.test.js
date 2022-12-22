164045abb78999bd9b94d9d4908b365950fd2cf8
Rico Rodriquez Collins <rico@carbonfive.com>
2022-12-21 21:02:38 -0800

    WIP - detox test

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -7,17 +7,17 @@ describe("Example", () => {
     await device.reloadReactNative();
   });
 
-  it("should have welcome screen", async () => {
-    await expect(element(by.id("welcome"))).toBeVisible();
-  });
+  // it("should have welcome screen", async () => {
+  //   await expect(element(by.id("welcome"))).toBeVisible();
+  // });
 
   it("should show hello screen after tap", async () => {
-    await element(by.id("hello_button")).tap();
-    await expect(element(by.text("Hello!!!"))).toBeVisible();
+    // await element(by.id("hello_button")).tap();
+    await expect(element(by.text("HEALING SEQUENCES"))).toBeVisible();
   });
 
-  it("should show world screen after tap", async () => {
-    await element(by.id("world_button")).tap();
-    await expect(element(by.text("World!!!"))).toBeVisible();
-  });
+  // it("should show world screen after tap", async () => {
+  //   await element(by.id("world_button")).tap();
+  //   await expect(element(by.text("World!!!"))).toBeVisible();
+  // });
 });
